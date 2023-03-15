8e61491b75fbc40f44279b0fe0f13b2ddd584167
Sito8943 <sito8943@gmail.com>
2023-03-14 19:28:53 -0700

    no force to number 0.4.1

--- array.js
+++ array.js
@@ -7,9 +7,9 @@
  */
 const sortBy = (array, attribute = "id", asc = false) => {
   return array.sort((itemA, itemB) => {
-    if (Number(itemA[attribute]) > Number(itemB[attribute]))
+    if (itemA[attribute] > itemB[attribute])
       return !asc ? -1 : 1;
-    if (Number(itemA[attribute]) < Number(itemB[attribute]))
+    if (itemA[attribute] < itemB[attribute])
       return !asc ? 1 : -1;
     return 0;
   });
