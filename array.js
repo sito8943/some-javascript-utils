7faf4bd78fb172b4467ba09bb590ddfceae1428e
Sito8943 <sito8943@gmail.com>
2023-08-27 05:26:04 -0700

    auxFunction

--- array.js
+++ array.js
@@ -9,11 +9,12 @@
 const sortBy = (array, attribute = "id", asc = false, auxFunction) => {
   return array.sort((itemA, itemB) => {
     if (auxFunction) {
-      console.assert(auxFunction(itemA[attribute]) !== undefined, "auxFunction should return something");
-      if (auxFunction(itemA[attribute]) > auxFunction(itemB[attribute]))
-        return !asc ? -1 : 1;
-      if (auxFunction(itemA[attribute]) < auxFunction(itemB[attribute]))
-        return !asc ? 1 : -1;
+      console.assert(
+        auxFunction(itemA[attribute]) !== undefined,
+        "auxFunction should return something"
+      );
+      if (auxFunction(itemA) > auxFunction(itemB)) return !asc ? -1 : 1;
+      if (auxFunction(itemA) < auxFunction(itemB)) return !asc ? 1 : -1;
       return 0;
     } else {
       if (itemA[attribute] > itemB[attribute]) return !asc ? -1 : 1;
