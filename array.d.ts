192d180a567b6db1e0b82d203868b0617ea9207d
Sito <sito8943@gmail.com>
2025-08-11 10:50:15 -0400

    moving types to different proy

--- array.d.ts
+++
@@ -1,9 +0,0 @@
-// types/some-javascript-utils/array.d.ts
-declare module "some-javascript-utils/array" {
-  export function sortBy<T extends Record<string, any>>(
-    array: T[],
-    attribute?: keyof T & string,
-    asc?: boolean,
-    auxFunction?: ((item: T) => any) | null
-  ): T[];
-}
